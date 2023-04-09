b8dce7120fbc7ae3ea142941c0e1c05e3bdcf35e
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-09 14:59:45 +1000

    Refactor Maybe type

---
+++ MaybeResult/Maybe.cs
@@ -0,0 +1,27 @@
+﻿namespace MaybeResult.Maybe
+{
+    public abstract class Maybe<T>
+    {
+        protected internal Maybe() { }
+
+        public static implicit operator Maybe<T>(T value) => Success(value);
+
+        public static Maybe<T> Success(T value) => new Success<T>(value);
+        public static Maybe<T> Failure() => new Failure<T>();
+    }
+
+    public sealed class Success<T> : Maybe<T>
+    {
+        internal Success(T value) : base()
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+
+    public sealed class Failure<T> : Maybe<T>
+    {
+        internal Failure() : base() { }
+    }
+}
