f620574feab380ea1b1739984a7b1e4b885ee01d
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-10 12:07:32 +1000

    Add Result and Error types

---
+++ MaybeResult/Error.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MaybeResult.Result
+{
+    public class Error : IEquatable<Error>
+    {
+        public Error(string code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public string Code { get; }
+        public string Message { get; }
+
+        public static bool operator ==(Error first, Error second)
+        {
+            return first != null && second != null && first.Equals(second);
+        }
+
+        public static bool operator !=(Error first, Error second)
+        {
+            return !(first == second);
+        }
+
+        public bool Equals(Error other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (other.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return other.Code == Code && other.Message == Message;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Error error = obj as Error;
+            if (error == null)
+            {
+                return false;
+            }
+
+            return error.Code == Code && error.Message == Message;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Code, Message).GetHashCode() * 79;
+        }
+    }
+}
---
+++ MaybeResult/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace MaybeResult.Result
+{
+    public abstract class Result<T>
+    {
+        protected internal Result() { }
+
+        public static implicit operator Result<T>(T value) => Success(value);
+
+        public static Result<T> Success(T value) => new Success<T>(value);
+        public static Result<T> Failure(Error error) => new Failure<T>(error);
+    }
+
+    public sealed class Success<T> : Result<T>
+    {
+        internal Success(T value) : base()
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+
+    public sealed class Failure<T> : Result<T>
+    {
+        internal Failure(Error error) : base()
+        {
+            Error = error;
+        }
+
+        public Error Error { get; }
+    }
+}
