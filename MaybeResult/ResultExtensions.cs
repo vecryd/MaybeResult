4a346dd69f3c4d8e8363ce53ac436ed29105b7b1
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-14 00:38:04 +1000

    Remove Result extensions methods with Func delegate and add parameters to Action delegates

--- MaybeResult/ResultExtensions.cs
+++ MaybeResult/ResultExtensions.cs
@@ -1,39 +1,25 @@
 ﻿using System;
 
-namespace MaybeResult.Result.Extensions
+namespace MaybeResult.ResultMonad.Extensions
 {
     public static class ResultExtensions
     {
-        public static Result<T> OnSuccess<T>(this Result<T> result, Func<Result<T>> func)
-        {
-            if (result is Failure<T> failure)
-            {
-                return failure;
-            }
-
-            return func.Invoke();
-        }
-
-        public static Result<T> OnSuccess<T>(this Result<T> result, Func<T, Result<T>> func)
-        {
-            if (result is Failure<T> failure)
-            {
-                return failure;
-            }
-
-            Success<T> success = (Success<T>)result;
-
-            return func.Invoke(success.Value);
-        }
-
         public static Result<T> OnSuccess<T>(this Result<T> result, Action action)
         {
-            if (result is Failure<T> failure)
+            if (result is Success<T>)
             {
-                return failure;
+                action.Invoke();
             }
 
-            action.Invoke();
+            return result;
+        }
+
+        public static Result<T> OnSuccess<T>(this Result<T> result, Action<Success<T>> action)
+        {
+            if (result is Success<T> success)
+            {
+                action.Invoke(success);
+            }
 
             return result;
         }
@@ -48,9 +34,14 @@ namespace MaybeResult.Result.Extensions
             return result;
         }
 
-        public static Result<T> OnBoth<T>(this Result<T> result, Func<Result<T>> func)
+        public static Result<T> OnFailure<T>(this Result<T> result, Action<Failure<T>> action)
         {
-            return func.Invoke();
+            if (result is Failure<T> failure)
+            {
+                action.Invoke(failure);
+            }
+
+            return result;
         }
 
         public static Result<T> OnBoth<T>(this Result<T> result, Action action)
