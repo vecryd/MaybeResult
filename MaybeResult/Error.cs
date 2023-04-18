080fa821c116cdedd139c49fc46f9ec9511018fc
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-18 18:13:02 +1000

    Rename Bind method to GetFormattedError

--- MaybeResult/Error.cs
+++ MaybeResult/Error.cs
@@ -2,6 +2,6 @@
 
 public record Error(string Code, string Message)
 {
-    public virtual Error Bind(params object[] parameters) =>
+    public virtual Error GetFormattedError(params object[] parameters) =>
         this with { Message = string.Format(Message, parameters) };
 }
