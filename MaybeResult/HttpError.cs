a1e054bc04c1dd06c11a95840d91474155c380ed
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-18 18:16:33 +1000

    Add an HttpError type

---
+++ MaybeResult/HttpError.cs
@@ -0,0 +1,8 @@
+﻿namespace MaybeResult.ResultMonad;
+
+public record HttpError(string Code, string Message, int StatusCode)
+    : Error(Code, Message)
+{
+    public override HttpError GetFormattedError(params object[] parameters) =>
+        this with { Message = string.Format(Message, parameters) };
+}
