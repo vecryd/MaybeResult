7116e16b307dd7efd1def5e35e0753550241d123
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-20 21:35:43 +1000

    Add Maybe extensions methods

---
+++ MaybeResult/MaybeMonad/Extensions/MaybeExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace MaybeResult.MaybeMonad.Extensions;
+
+public static class MaybeExtensions
+{
+    public static Maybe<T> OnSuccess<T>(this Maybe<T> maybe, Action action)
+    {
+        if (maybe is Success<T>)
+        {
+            action.Invoke();
+        }
+
+        return maybe;
+    }
+
+    public static Maybe<T> OnSuccess<T>(this Maybe<T> maybe, Action<T> action)
+    {
+        if (maybe is Success<T> success)
+        {
+            action.Invoke(success.Value);
+        }
+
+        return maybe;
+    }
+
+    public static Maybe<T> OnFailure<T>(this Maybe<T> maybe, Action action)
+    {
+        if (maybe is Failure<T>)
+        {
+            action.Invoke();
+        }
+
+        return maybe;
+    }
+
+    public static Maybe<T> OnBoth<T>(this Maybe<T> maybe, Action action)
+    {
+        action.Invoke();
+
+        return maybe;
+    }
+}
