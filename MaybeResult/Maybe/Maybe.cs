e2a2e566a5d9242ea2aedee9aa5cf160a863daea
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-04-24 18:00:26 +1000

    Refactor the Bind method and add the Map method, move the static Bind methods to a non-generic class Maybe with the new Map methods

---
+++ MaybeResult/Maybe/Maybe.cs
@@ -0,0 +1,88 @@
+﻿namespace MaybeResult;
+
+public abstract class Maybe
+{
+    public static Maybe<U> Bind<T, U>(Maybe<T> maybe, Func<T, Maybe<U>> binder) => maybe.Bind(binder);
+
+    public static Maybe<U> Bind<T1, T2, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Func<T1, T2, Maybe<U>> binder)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return binder.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value);
+    }
+
+    public static Maybe<U> Bind<T1, T2, T3, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Func<T1, T2, T3, Maybe<U>> binder)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return binder.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value);
+    }
+
+    public static Maybe<U> Bind<T1, T2, T3, T4, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Maybe<T4> maybe4, Func<T1, T2, T3, T4, Maybe<U>> binder)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3> || maybe4 is None<T4>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return binder.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value, ((Some<T4>)maybe4).Value);
+    }
+
+    public static Maybe<U> Bind<T1, T2, T3, T4, T5, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Maybe<T4> maybe4, Maybe<T5> maybe5, Func<T1, T2, T3, T4, T5, Maybe<U>> binder)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3> || maybe4 is None<T4> || maybe5 is None<T5>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return binder.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value, ((Some<T4>)maybe4).Value, ((Some<T5>)maybe5).Value);
+    }
+
+    public static Maybe<U> Map<T, U>(Maybe<T> maybe, Func<T, U> mapper) => maybe.Map(mapper);
+
+    public static Maybe<U> Map<T1, T2, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Func<T1, T2, U> mapper)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return mapper.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value);
+    }
+
+    public static Maybe<U> Map<T1, T2, T3, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Func<T1, T2, T3, U> mapper)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return mapper.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value);
+    }
+
+    public static Maybe<U> Map<T1, T2, T3, T4, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Maybe<T4> maybe4, Func<T1, T2, T3, T4, U> mapper)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3> || maybe4 is None<T4>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return mapper.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value, ((Some<T4>)maybe4).Value);
+    }
+
+    public static Maybe<U> Map<T1, T2, T3, T4, T5, U>(Maybe<T1> maybe1, Maybe<T2> maybe2, Maybe<T3> maybe3, Maybe<T4> maybe4, Maybe<T5> maybe5, Func<T1, T2, T3, T4, T5, U> mapper)
+    {
+        if (maybe1 is None<T1> || maybe2 is None<T2> || maybe3 is None<T3> || maybe4 is None<T4> || maybe5 is None<T5>)
+        {
+            return Maybe<U>.None();
+        }
+
+        return mapper.Invoke(((Some<T1>)maybe1).Value, ((Some<T2>)maybe2).Value, ((Some<T3>)maybe3).Value, ((Some<T4>)maybe4).Value, ((Some<T5>)maybe5).Value);
+    }
+}
---
+++ MaybeResult/Maybe/Maybe`1.cs
@@ -0,0 +1,27 @@
+﻿namespace MaybeResult;
+
+public abstract class Maybe<T>
+{
+    internal Maybe() { }
+
+    public static implicit operator Maybe<T>(T value) => Some(value);
+
+    public static Maybe<T> Some(T value) => new Some<T>(value);
+    public static Maybe<T> None() => new None<T>();
+
+    public T? GetValueOrDefault() => this is Some<T> some ? some.Value : default;
+
+    public Maybe<U> Bind<U>(Func<T, Maybe<U>> binder) => this switch
+    {
+        Some<T> some => binder.Invoke(some.Value),
+        None<T> => Maybe<U>.None(),
+        _ => throw new InvalidOperationException()
+    };
+
+    public Maybe<U> Map<U>(Func<T, U> mapper) => this switch
+    {
+        Some<T> some => mapper.Invoke(some.Value),
+        None<T> => Maybe<U>.None(),
+        _ => throw new InvalidOperationException()
+    };
+}
